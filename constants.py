eb033dad7dddb571ea14f13f8214494d3bb2efd2
w1624401 <34352629+w1624401@users.noreply.github.com>
2019-01-31 10:41:38 +0000

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,5 @@
+
+# Reserved keywords for programming language
+KEYWORDS = ["function", "class", "if", "for", "else", "True", "False", "nil", "printf", "bool", "int", "str"]
+BUILT_IN_FUNCTIONS = ["printf"]
+DATATYPE = ["bool", "int", "str"]
---
+++ test.c
@@ -0,0 +1,22 @@
+int a = 11;
+str name = "Suka";
+bool isMale = True;
+int num = 10 * 10 + 10;
+str full_name = name + " " + name;
+str gang;
+
+
+
+for int x = 0 :: > 10 :: + 1 {
+    printf "Hi";
+}
+
+if a == 11 {
+	printf "Perfect";
+	if name != "Suka" {
+		printf "Not good";
+	}
+}
+
+
+
